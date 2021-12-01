acb87a8bbcab37c2e021d058bb957df749fdd5bc
TakumiHANAUE <40872702+TakumiHANAUE@users.noreply.github.com>
2021-12-01 14:50:24 +0000

    add day01 part1

---
+++ day01/day01_part1.nim
@@ -0,0 +1,16 @@
+# https://adventofcode.com/2021/day/1
+
+import system
+import strutils
+
+var inputFile: File = open("day01_input.txt", fmRead);
+
+var count: int = 0
+var prev_measurement: int = high(int)
+for line in inputFile.lines:
+    var measurement: int = parseInt(line)
+    if measurement > prev_measurement:
+        inc(count)
+    prev_measurement = measurement
+
+echo "the number of times a depth measurement increases: ", count
